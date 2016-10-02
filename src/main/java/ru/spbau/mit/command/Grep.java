ab0182e0a173a2022c237badf902003478db0b6c
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-10-02 23:23:17 +0300

    new branch i hope

---
+++ src/main/java/ru/spbau/mit/command/Grep.java
@@ -0,0 +1,11 @@
+package ru.spbau.mit.command;
+
+/**
+ * Created by Эдгар on 02.10.2016.
+ */
+public class Grep implements Command {
+    @Override
+    public String execute(String input) {
+        return "";
+    }
+}
