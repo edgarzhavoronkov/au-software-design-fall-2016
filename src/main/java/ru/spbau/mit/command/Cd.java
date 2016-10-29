481ef0c100f3c5bceb69b846613fce51a6d71380
Golovanov <Sergey_XG@mail.ru>
2016-10-29 16:06:17 +0300

    fix cd

--- src/main/java/ru/spbau/mit/command/Cd.java
+++ src/main/java/ru/spbau/mit/command/Cd.java
@@ -1,8 +1,15 @@
 package ru.spbau.mit.command;
 
+import java.io.File;
+
 public class Cd implements Command {
     @Override
     public String execute(String input) {
+        File directory = new File(input);
+        if (!directory.exists() || !directory.isDirectory()) {
+            return "Directory is not found";
+        }
+
         System.setProperty("user.dir", input);
         return input;
     }
