3e53ebb53953e19dc4880800f3d99ab5f4739186
Golovanov <Sergey_XG@mail.ru>
2016-12-11 11:28:14 +0300

    small fix

--- src/main/java/ru/spbau/mit/command/Cd.java
+++ src/main/java/ru/spbau/mit/command/Cd.java
@@ -4,7 +4,16 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * Implementation of Command interface for Cd.
+ * Command change the current working directory to a specific Folder.
+ */
 public class Cd implements Command {
+    /**
+     * {@link Cd}'s .execute implementation
+     * @param input String with directory path
+     * @return String with user directory path
+     */
     @Override
     public String execute(String input) {
         Path workDirectory = Paths.get(System.getProperty("user.dir"));
@@ -13,7 +22,7 @@ public class Cd implements Command {
                                       .normalize()
                                       .toFile();
         if (!directory.exists() || !directory.isDirectory()) {
-            return "Directory is not found";
+            return "Directory not found";
         }
 
         System.setProperty("user.dir", directory.toString());
--- src/main/java/ru/spbau/mit/command/Ls.java
+++ src/main/java/ru/spbau/mit/command/Ls.java
@@ -1,12 +1,31 @@
 package ru.spbau.mit.command;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+/**
+ * Implementation of Command interface for Ls.
+ * Command shows information about the files.
+ */
 public class Ls implements Command {
+    /**
+     * {@link Ls}'s .execute implementation
+     * @param input Empty string or string with directory path
+     * @return String with file names
+     */
     @Override
     public String execute(String input) {
-        String currentDirectory = System.getProperty("user.dir");
-        String[] files = new File(currentDirectory).list();
+        String currentDirectory = input.equals("") ? System.getProperty("user.dir") : input;
+        Path workDirectory = Paths.get(System.getProperty("user.dir"));
+        File directory = workDirectory.resolve(currentDirectory)
+                                        .toAbsolutePath()
+                                        .normalize()
+                                        .toFile();
+        if (!directory.exists() || !directory.isDirectory()) {
+            return "Directory not found";
+        }
+        String[] files = directory.list();
         return String.join("\n", files);
     }
 }
